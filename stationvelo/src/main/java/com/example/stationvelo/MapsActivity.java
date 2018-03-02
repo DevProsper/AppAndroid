3526e89f380c737c6088da649273aac2df436475
DevProsper <prosperngouari38@gmail.com>
2018-03-02 21:48:53 +0100

    Affichage des markeurs sur une carte

--- stationvelo/src/main/java/com/example/stationvelo/MapsActivity.java
+++ stationvelo/src/main/java/com/example/stationvelo/MapsActivity.java
@@ -2,18 +2,24 @@ package com.example.stationvelo;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.example.stationvelo.model.beans.Station;
 import com.example.stationvelo.model.webservice.GetStationAT;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.util.ArrayList;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GetStationAT.GetStationATResult {
 
     private GoogleMap mMap;
+    private ArrayList<Station> stations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +29,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-       new GetStationAT().execute();
+        stations = new ArrayList<Station>();
+       new GetStationAT(this).execute();
     }
 
 
@@ -42,8 +48,53 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        rafraichirCarte();
+    }
+
+    public void rafraichirCarte(){
+        if (mMap == null){
+            return;
+        }
+        mMap.clear();
+
+        if (!stations.isEmpty()){
+
+            //Afficher ou centrer une carte après l'animation
+            LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+            for (Station station : stations){
+                final MarkerOptions marker = new MarkerOptions();
+                LatLng latLng = new LatLng(station.getPosition().getLat(), station.getPosition().getLng());
+
+                builder.include(latLng);
+
+                marker.position(latLng);
+                marker.title(station.getName());
+
+                mMap.addMarker(marker).setTag(station);
+            }
+
+            int padding = 100;
+            mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(
+                    builder.build(), padding
+            ));
+        }
+    }
+
+    @Override
+    public void stationChargees(ArrayList<Station> stations) {
+        this.stations.clear();
+        this.stations.addAll(stations);
+        rafraichirCarte();
+    }
+
+    @Override
+    public void getStationATResultErreur(Exception e) {
+        e.printStackTrace();
+        Toast.makeText(MapsActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
     }
 }
--- stationvelo/src/main/java/com/example/stationvelo/model/webservice/GetStationAT.java
+++ stationvelo/src/main/java/com/example/stationvelo/model/webservice/GetStationAT.java
@@ -2,18 +2,51 @@ package com.example.stationvelo.model.webservice;
 
 import android.os.AsyncTask;
 
+import com.example.stationvelo.model.beans.Station;
+
+import java.util.ArrayList;
+
 /**
  * Created by DevProsper on 02/03/2018.
  */
 
 public class GetStationAT extends AsyncTask {
+
+    private ArrayList<Station> stations;
+    private Exception exception;
+    private GetStationATResult getStationATResult;
+
+    public GetStationAT(GetStationATResult getStationATResult) {
+        this.getStationATResult = getStationATResult;
+    }
+
     @Override
     protected Object doInBackground(Object[] objects) {
         try {
-            WSUtils.getStation();
+            stations =  WSUtils.getStation();
         }catch (Exception e){
-            e.printStackTrace();
+            exception = e;
         }
         return null;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+
+        if (getStationATResult != null){
+
+            if (exception !=null){
+                getStationATResult.getStationATResultErreur(exception);
+            }else{
+                getStationATResult.stationChargees(stations);
+            }
+        }
+
+    }
+
+    public interface GetStationATResult{
+        void stationChargees(ArrayList<Station> stations);
+        void getStationATResultErreur(Exception e);
     }
 }
