5fcd76d0b6381c6845eb92cfbd66045175ad49b4
DevProsper <prosperngouari38@gmail.com>
2018-03-02 23:19:21 +0100

    Affichage de la fenêtre lors du click avec le bouton

--- stationvelo/src/main/java/com/example/stationvelo/MapsActivity.java
+++ stationvelo/src/main/java/com/example/stationvelo/MapsActivity.java
@@ -2,25 +2,31 @@ package com.example.stationvelo;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.stationvelo.model.beans.Station;
 import com.example.stationvelo.model.webservice.GetStationAT;
+import com.example.stationvelo.vue.ClusterIconRenderer;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.clustering.ClusterManager;
 
 import java.util.ArrayList;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GetStationAT.GetStationATResult {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GetStationAT.GetStationATResult, ClusterManager.OnClusterItemClickListener<Station>, GoogleMap.InfoWindowAdapter {
 
     private GoogleMap mMap;
     private ArrayList<Station> stations;
+    private Station stationClick;
 
     private ClusterManager<Station> mClusterManager;
 
@@ -33,6 +39,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         stations = new ArrayList<>();
+
+        stationClick = null;
+
        new GetStationAT(this).execute();
     }
 
@@ -56,9 +65,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
         mClusterManager = new ClusterManager<>(this, mMap);
+        //Modifier l'icon du marqueur
+        mClusterManager.setRenderer(new ClusterIconRenderer(this, mMap, mClusterManager));
+
+        mClusterManager.setOnClusterItemClickListener(this);
+        mClusterManager.getMarkerCollection().setOnInfoWindowAdapter(this);
 
         mMap.setOnCameraIdleListener(mClusterManager);//Permet d'être au courant au moment ou le mouvement de camera se termine
         mMap.setOnMarkerClickListener(mClusterManager);//Clic sur le markeur
+
+        mMap.setInfoWindowAdapter(mClusterManager.getMarkerManager());
 
         rafraichirCarte();
     }
@@ -100,5 +116,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void getStationATResultErreur(Exception e) {
         e.printStackTrace();
         Toast.makeText(MapsActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+    }
+
+    //Click sur un cluster
+    @Override
+    public boolean onClusterItemClick(Station station) {
+        stationClick = station;
+        return false;
+    }
+
+
+    //Pour gerer toute la fenêtre
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    //pour gerer l'interieur de la fenêtre
+    @Override
+    public View getInfoContents(Marker marker) {
+        if (stationClick == null){
+            return null;
+        }
+        View view = LayoutInflater.from(this).inflate(R.layout.marker_station, null);
+
+        TextView tv_nom = (TextView)view.findViewById(R.id.tv_nom);
+        TextView tv_adress = (TextView)view.findViewById(R.id.tv_adress);
+        TextView tv_velo_dispo = (TextView)view.findViewById(R.id.tv_velo_dispo);
+        TextView tv_velo_vide = (TextView)view.findViewById(R.id.tv_velo_vide);
+
+        tv_nom.setText(stationClick.getName());
+        tv_adress.setText(stationClick.getAddress());
+        tv_velo_dispo.setText(stationClick.getAvailable_bikes() + "");
+        tv_velo_vide.setText(stationClick.getAvailable_bike_stands() + "");
+
+        return view;
     }
 }
